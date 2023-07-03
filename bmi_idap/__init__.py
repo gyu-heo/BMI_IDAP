d7a6cc6c43e824d20820e9cc10d34cb00871f0a3
RichieHakim <RichHakim@gmail.com>
2023-07-03 19:36:25 -0400

    clean up

--- bmi_idap/__init__.py
+++ bmi_idap/__init__.py
@@ -4,6 +4,8 @@ __all__ = [
     
 ]
 
+__version__ = '0.1.0'
+
 for pkg in __all__:
     exec('from . import ' + pkg)
 del pkg
--- setup.py
+++ setup.py
@@ -1,30 +1,42 @@
-from setuptools import find_packages, setup
+from pathlib import Path
+
+from distutils.core import setup
+import copy
+
+dir_parent = Path(__file__).parent
+
+def read_requirements():
+    with open(str(dir_parent / "requirements.txt"), "r") as req:
+        content = req.read()  ## read the file
+        requirements = content.split("\n") ## make a list of requirements split by (\n) which is the new line character
+
+    ## Filter out any empty strings from the list
+    requirements = [req for req in requirements if req]
+    ## Filter out any lines starting with #
+    requirements = [req for req in requirements if not req.startswith("#")]
+    ## Remove any commas, quotation marks, and spaces from each requirement
+    requirements = [req.replace(",", "").replace("\"", "").replace("\'", "").strip() for req in requirements]
+
+    return requirements
+
+deps_all = read_requirements()
+
+## Get version number
+with open(str(dir_parent / "bmi_idap" / "__init__.py"), "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.split("=")[1].strip().replace("\"", "").replace("\'", "")
+            break
+
 
 setup(
     name='bmi_idap',
-    packages=find_packages(),
-    version='0.1.0',
+    packages=['bmi_idap'],
+    version=version,
     description="BMI interday analysis pipeline",
     author='Rich Hakim',
     license='MIT',
-    install_requires=[
-    # 'numpy==1.18.3',
-    # 'jupyterlab',
-    # 'opencv-python',
-    # 'imageio==2.9.0',
-    # 'matplotlib',
-    # 'scikit-learn',
-    # 'scikit-image',
-    # 'librosa',
-    # 'pyyaml',
-    # 'imageio-ffmpeg',
-    # 'tqdm',
-    # 'h5py==2.10.0',
-    # 'pynwb',
-    # 'ipywidgets',
-    # 'pytest',
-    # 'Pillow==7.2.0'
-    ]
+    install_requires=deps_all,
 )
 
 # from distutils.core import setup
